beed45c60899fed86fda9080d1394d2f07c646db
Andrey Tatarinov <a@tatarinov.co>
2020-03-15 19:41:50 +0300

    another attempt at package data

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ setuptools.setup(
     name='e8_dash_app',
     use_scm_version=True,
     packages=[
-        'e8_dash_app'
+        'e8_dash_app',
+        'e8_dash_app.assets',
+        'e8_dash_app.assets.css',
+        'e8_dash_app.assets.fonts',
+        'e8_dash_app.assets.images',
+        'e8_dash_app.assets.js',
     ],
-    include_package_data=True,
+    package_data={
+        "": ["*.css", "*.js", "*.png", "*.woff", "*.woff2", "*.ico"]
+    }
 )
